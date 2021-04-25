49f0c3a00c99d41f32b786808ebc8e156c76a956
DMC2712 <chavdadhruv0505@gmail.com>
2021-04-25 10:51:28 +0530

    useEffect Started Added Ue1 App will now have only Ue components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import "./App.css";
-import Us1 from "./useStateExamples/Us1";
-import Us2 from "./useStateExamples/Us2";
-import Us3 from "./useStateExamples/Us3";
-import Us4 from "./useStateExamples/Us4";
+import Ue1 from "./useEffectExamples/Ue1";
 
 function App() {
   return (
     <div className="App">
-      <p>Incorrect</p>
-      <Us1 />
-      <p>Correct</p>
-      <Us2 />
-      <p>UseState With Object</p>
-      <Us3 />
-      <p>UseState with Array</p>
-      <Us4/>
+      <p>useEffect Examples</p>
+      <Ue1 />
     </div>
   );
 }
---
+++ src/useEffectExamples/Ue1.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+import { Button, Typography } from "@material-ui/core";
+
+// useEffect Example
+// useEffect Renders everytime a component is rendered
+
+function Ue1() {
+  const [cnt, setCnt] = useState(0);
+  useEffect(() => {
+    if (cnt === 1) {
+      document.title = `${cnt} time clicked!`;
+    } else {
+      document.title = `${cnt} times clicked!`;
+    }
+  });
+
+  return (
+    <React.Fragment>
+      <Typography variant="h5" gutterbottom>
+        Adder
+      </Typography>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => {
+          setCnt((x) => x + 1);
+        }}
+      >
+        {cnt}
+      </Button>
+    </React.Fragment>
+  );
+}
+
+export default Ue1;
